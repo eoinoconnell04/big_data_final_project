f38da95fc18ecf476bdcfd2bafe11b9bf3b42eba
Eoin OConnell <eoinoconnell04@gmail.com>
2024-05-09 09:50:01 -0700

    updating files

--- services/postgres/schema.sql
+++ services/postgres/schema.sql
@@ -6,23 +6,23 @@ BEGIN;
 
 CREATE TABLE users (
 	id_users BIGSERIAL PRIMARY KEY,
-	username TEXT UNIQUE NOT NULL,
+	username TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL,
 	bio TEXT
+);
+
+CREATE TABLE urls (
+        id_urls BIGSERIAL PRIMARY KEY,
+        -- id_tweets BIGINT UNIQUE REFERENCES tweets(id_tweets),
+        url TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE tweets (
 	id_tweets BIGSERIAL PRIMARY KEY,
 	id_users BIGINT NOT NULL REFERENCES users(id_users),
-	id_urls BIGINT UNIQUE NOT NULL REFERENCES urls(id_urls),
+	id_urls BIGINT NOT NULL UNIQUE REFERENCES urls(id_urls),
 	text TEXT,
 	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE urls (
-	id_urls BIGSERIAL PRIMARY KEY,
-	-- id_tweets BIGINT UNIQUE REFERENCES tweets(id_tweets),
-	url TEXT UNIQUE NOT NULL
 );
 
 COMMIT;
