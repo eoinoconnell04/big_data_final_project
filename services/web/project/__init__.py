3488bbe2a0a52f5d7805000eaafd62b1bd7a6c9a
Eoin OConnell <eoinoconnell04@gmail.com>
2024-05-13 11:49:12 -0700

    updating files

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -5,7 +5,7 @@ from flask import (
     jsonify,
     send_from_directory,
     request,
-    render_template
+    render_template,
     make_response
 )
 from flask_sqlalchemy import SQLAlchemy
@@ -31,7 +31,7 @@ def root():
 
     username = request.cookies.get('username')
     password = request.cookies.get('password')
-    good_creds = check_creds(username,passowrd)
+    good_creds = check_creds(username,password)
 
 
     return render_template('root.html', logged_in=good_creds)
@@ -59,8 +59,11 @@ def login():
 def logout():
     
     #delete cookies
+    response = make_response(render_template('logout.html'))
+    response.delete_cookie('username')
+    response.delete_cookie('password')
 
-    return render_template('logout.html') 
+    return response 
 
 @app.route('/create_account')
 def create_account():
@@ -68,28 +71,109 @@ def create_account():
     username = request.form.get('username')
     password = request.form.get('password')
     password2 = request.form.get('password2')
-
-    if (password != password2):
-        return render_template('create_account.html', mismatch=True, taken=False)
-    elif check_taken(username):
-        return render_template('create_account.html', mismatch=False, taken=True)
+    if username is not None and password is not None and password2 is not None:
+        if (password != password2):
+            return render_template('create_account.html', mismatch=True, taken=False, empty=False)
+        elif check_taken(username):
+            return render_template('create_account.html', mismatch=False, taken=True, empty=False)
+        else:
+            return render_template('create_account.html', mismatch=False, taken=False, empty=False)
     else:
-        return render_template('create_account.html', mismatch=False, taken=False)
+        return render_template('create_account.html', mismatch=False, taken=False, empty=True)
 
-@app.route('/create_message')
+@app.route('/create_message', methods=['GET', 'POST'])
 def create_message():
-    return render_template('create_message.html')
+
+    message = request.form.get('message')
+    
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_creds = check_creds(username,password)
+
+    if good_creds and message is not None:
+        insert_tweet(message, username)
+        return render_template('create_message.html',logged_in=good_creds, message_inserted=True)
+
+    return render_template('create_message.html',logged_in=good_creds, message_inserted=False)
 
 @app.route('/search')
 def search():
-    return render_template('search.html')
+
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_creds = check_creds(username,password)
+
+    search = request.form.get('search')
+
+    return render_template('search.html',logged_in=good_creds)
 
 
 
 #helper functions:
 def check_creds(username, password):
-    return true
+    
+    sql = sqlalchemy.sql.text('''
+        SELECT * FROM users
+        WHERE username = :username
+        AND password = :password;
+        ''')
+
+    res = connection.execute(sql, {
+        'username': username,
+        'password': password
+    })
+
+    # if there is a user with the corresponding username and password, then the query will have a non empty result
+    if res.fetchone() is None:
+        return False
+    else:
+        return True
 
 def check_taken(username):
-    return false
+    sql = sqlalchemy.sql.text('''
+        SELECT * FROM users
+        WHERE username = :username;
+        ''')
+
+    res = connection.execute(sql, {
+        'username': username
+    })
+    
+    # if there is a user with the corresponding username then it query qill have a non empty result (username taken)
+    if res.fetchone() is None:
+        return False
+    else:
+        return True
+
+def insert_tweet(text, username):
+    
+    sql = sqlalchemy.sql.text('''
+        SELECT id_users FROM users
+        WHERE username = :username;
+        ''')
+
+    result_id_users = connection.execute(sql, {'username': username})
+    id_users = result_id_users.fetchone()[0]  # Fetch the result and extract the value
+
+    sql = sqlalchemy.sql.text('''
+        SELECT MIN(u.id_urls)               
+        FROM urls u
+        LEFT JOIN tweets t ON u.id_urls = t.id_urls
+        WHERE t.id_urls IS NULL;
+        ''')
+
+    result_id_urls = connection.execute(sql)
+    id_urls = result_id_urls.fetchone()[0]  # Fetch the result and extract the value
+
+    sql = sqlalchemy.sql.text(
+            '''
+            INSERT INTO tweets (text, id_users, id_urls) 
+            VALUES (:text, :id_users, :id_urls)
+            ''')
+
+    sql = sql.bindparams(text=text,
+        id_users=id_users,
+        id_urls=id_urls)
+    connection.execute(sql)
+
 
