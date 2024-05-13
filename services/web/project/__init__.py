b31985fd82b9b749e0e5f1f4664303cd4ef87a88
Eoin OConnell <eoinoconnell04@gmail.com>
2024-05-12 18:53:46 -0700

    adding web files

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -5,52 +5,89 @@ from flask import (
     jsonify,
     send_from_directory,
     request,
+    render_template
+    make_response
 )
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
-app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
+engine = sqlalchemy.create_engine("postgresql://postgres:pass@postgres:5432", connect_args={
+    'application_name': '__init__.py',
+    })
+connection = engine.connect()
 
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+
+@app.route('/')
+def root():
+
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_creds = check_creds(username,passowrd)
 
 
-@app.route("/static/<path:filename>")
-def staticfiles(filename):
-    return send_from_directory(app.config["STATIC_FOLDER"], filename)
+    return render_template('root.html', logged_in=good_creds)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    approved = check_creds(username,password)
+    if username is None:
+        return render_template('login.html', bad_creds=False)
+    else:
+        if approved:
+
+            # create cookie
+            template = render_template('login.html', bad_creds=False, logged_in=True)
+            response = make_response(template)
+            response.set_cookie('username', username)
+            response.set_cookie('password', password)
+            return response
+        else:
+            return render_template('login.html', bad_creds=True) 
+
+@app.route('/logout')
+def logout():
+    
+    #delete cookies
+
+    return render_template('logout.html') 
+
+@app.route('/create_account')
+def create_account():
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if (password != password2):
+        return render_template('create_account.html', mismatch=True, taken=False)
+    elif check_taken(username):
+        return render_template('create_account.html', mismatch=False, taken=True)
+    else:
+        return render_template('create_account.html', mismatch=False, taken=False)
+
+@app.route('/create_message')
+def create_message():
+    return render_template('create_message.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
 
 
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
 
+#helper functions:
+def check_creds(username, password):
+    return true
 
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return """
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
+def check_taken(username):
+    return false
+
+app.run()
+
